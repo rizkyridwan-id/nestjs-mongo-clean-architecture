fafa0f7db9180dce92dfedb8fde5a9539f8f9d85
riz <mr.ridwan.thekeeper@gmail.com>
2024-02-20 10:50:49 +0700

    fix(response-dto) change the type into generic

--- src/core/base/http/response.dto.base.ts
+++ src/core/base/http/response.dto.base.ts
@@ -1,7 +1,14 @@
 import { IsNumber, IsObject, IsString } from 'class-validator';
 
-export class ResponseDto {
-  constructor({ status, data = {}, message = '', count = 0 }: ResponseDto) {
+interface IResponseDto<T = { [k: string]: any }> {
+  status: number;
+  data?: T;
+  message?: string;
+  count?: number;
+}
+
+export class ResponseDto<T = { [k: string]: any }> implements IResponseDto<T> {
+  constructor({ status, data, message = '', count = 0 }: IResponseDto<T>) {
     this.status = status;
     this.data = data;
     this.message = message;
@@ -12,7 +19,7 @@ export class ResponseDto {
   status: number;
 
   @IsObject()
-  data?: { [k: string]: any };
+  data?: T;
 
   @IsString()
   message?: string;
