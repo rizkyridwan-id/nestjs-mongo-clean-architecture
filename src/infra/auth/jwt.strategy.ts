1643413e7328277c6e621b3700ab91f8c5a1c9ce
riz <mr.ridwan.thekeeper@gmail.com>
2024-02-20 10:53:17 +0700

    fix(jwt-strategy) add manual token expiration validation

--- src/infra/auth/jwt.strategy.ts
+++ src/infra/auth/jwt.strategy.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { EnvService } from '../config/env.service';
@@ -8,12 +8,15 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(envService: EnvService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: false,
+      ignoreExpiration: true,
       secretOrKey: envService.variables.jwtSecretKey,
     });
   }
 
   async validate(payload: any) {
+    if (payload.exp < Date.now() / 1000) {
+      throw new BadRequestException('Token is Expired');
+    }
     return { user_id: payload.sub };
   }
 }
